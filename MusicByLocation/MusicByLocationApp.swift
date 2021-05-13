51d823f27b955f63edcc7fcd97b7c8bd6bfceabb
Saahil <bahadur.s@etoncollege.org.uk>
2021-05-13 20:57:11 +0100

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Rakesh Bahadur on 13/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
